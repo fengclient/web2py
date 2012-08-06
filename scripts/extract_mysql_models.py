eb290c6124a6f3f10d6bebea4899bea897482823
fengclient <fengclient@gmail.com>
2012-08-06 21:42:09 +0800

    add primary key support

--- scripts/extract_mysql_models.py
+++ scripts/extract_mysql_models.py
@@ -19,6 +19,12 @@ If the script crashes this is might be due to that fact that the data_type_map d
 Please complete it, improve it and continue.
 
 Created by Falko Krause, minor modifications by Massimo Di Pierro and Ron McOuat
+
+2012-07-26: 
+Changed by fengclient: 
+1 force protocol=tcp on mysql and mysqldump
+2 set DAL options
+3 add primary key support
 '''
 import subprocess
 import re
@@ -52,7 +58,9 @@ data_type_map = dict(
         )
 
 def mysql(database_name, username, password):
+    print 'from gluon.dal import DAL, Field'
     p = subprocess.Popen(['mysql',
+                          '--protocol=tcp',
                           '--user=%s' % username,
                           '--password=%s'% password,
                           '--execute=show tables;',
@@ -62,11 +70,13 @@ def mysql(database_name, username, password):
                          stderr=subprocess.PIPE)
     sql_showtables, stderr = p.communicate()
     tables = [re.sub('\|\s+([^\|*])\s+.*', '\1', x) for x in sql_showtables.split()[1:]]
-    connection_string = "legacy_db = DAL('mysql://%s:%s@localhost/%s')"%(username, password, database_name)
+    connection_string = "legacy_db = DAL('mysql://%s:%s@localhost/%s',pool_size=10, db_codec='latin1',migrate=False, migrate_enabled=False)"\
+                        %(username, password, database_name)
     legacy_db_table_web2py_code = []
     for table_name in tables:
         #get the sql create statement
         p = subprocess.Popen(['mysqldump',
+                              '--protocol=tcp',
                               '--user=%s' % username,
                               '--password=%s' % password,
                               '--skip-add-drop-table',
@@ -81,7 +91,13 @@ def mysql(database_name, username, password):
             web2py_table_code = ''
             table_name = re.search('CREATE TABLE .(\S+). \(', sql_lines[0]).group(1)
             fields = []
+            auto_increment_fields = []
+            primary_key_fields = []
+            id_field = None
             for line in sql_lines[1:-1]:
+                hit_pk=re.search('PRIMARY KEY \((\S+)\)', line)
+                if hit_pk:
+                    primary_key_fields=re.findall('`(\S+?)`', line)
                 if re.search('KEY', line) or re.search('PRIMARY', line) or re.search(' ID', line) or line.startswith(')'):
                     continue
                 hit = re.search('(\S+)\s+(\S+)(,| )( .*)?', line)
@@ -89,7 +105,18 @@ def mysql(database_name, username, password):
                     name, d_type = hit.group(1), hit.group(2)
                     d_type = re.sub(r'(\w+)\(.*',r'\1',d_type)
                     name = re.sub('`','',name)
+                    if re.search('AUTO_INCREMENT',line):
+                        auto_increment_fields.append(name)
+                    fields.append((name,d_type))
+            for (name, d_type) in fields:
+                if name in primary_key_fields and name in auto_increment_fields and len(primary_key_fields)==1:
+                    id_field = name
+                    #i don't care d_type
+                    web2py_table_code += "\n    Field('%s','%s'),"%(name,'id')
+                else:
                     web2py_table_code += "\n    Field('%s','%s'),"%(name,data_type_map[d_type])
+            if primary_key_fields:
+                web2py_table_code += "\n    primarykey=[%s],"%(','.join(["'%s'"%(x) for x in primary_key_fields]))
             web2py_table_code = "legacy_db.define_table('%s',%s\n    migrate=False)"%(table_name,web2py_table_code)
             legacy_db_table_web2py_code.append(web2py_table_code)
     #----------------------------------------
